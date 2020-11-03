3eb4cf9586c7e9ee1aefa3961327a115f70ba85f
Ivan Tomicic <ivan.tomicic@fer.hr>
2020-11-03 16:29:52 +0100

    replaced int increment with uuid

--- dump-deg1t0cmdemhn7-202011031553.sql
+++ dump-deg1t0cmdemhn7-202011031553.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.4 (Ubuntu 12.4-1.pgdg16.04+1)
 -- Dumped by pg_dump version 13.0
 
--- Started on 2020-11-02 15:32:26
+-- Started on 2020-11-03 15:53:55
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -29,7 +29,7 @@ CREATE SCHEMA public;
 ALTER SCHEMA public OWNER TO icfjttdivtiins;
 
 --
--- TOC entry 3879 (class 0 OID 0)
+-- TOC entry 3876 (class 0 OID 0)
 -- Dependencies: 3
 -- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: icfjttdivtiins
 --
@@ -47,7 +47,7 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.administrator (
-    user_id integer NOT NULL
+    administrator_uuid character varying(32) NOT NULL
 );
 
 
@@ -62,30 +62,15 @@ CREATE TABLE public.app_user (
     email character varying(255) NOT NULL,
     password_hash character varying(128) NOT NULL,
     role character varying(20) NOT NULL,
-    user_id integer NOT NULL,
     oib character varying(11),
+    user_uuid character varying(32) NOT NULL,
+    CONSTRAINT app_user_check_uuid CHECK (((user_uuid)::text ~ '^[a-f0-9]{32}$'::text)),
     CONSTRAINT app_user_email_check CHECK (((email)::text ~* '^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$'::text)),
-    CONSTRAINT app_user_oib_check CHECK (((oib)::text ~ '^[0-9]{11}$'::text)),
-    CONSTRAINT app_user_role_check CHECK ((((role)::text = 'administrator'::text) OR ((role)::text = 'company'::text) OR ((role)::text = 'person'::text)))
+    CONSTRAINT app_user_oib_and_role_check CHECK ((((((role)::text = 'person'::text) OR ((role)::text = 'company'::text)) AND ((oib)::text ~ '^[0-9]{11}$'::text)) OR (((role)::text = 'administrator'::text) AND (oib IS NULL))))
 );
 
 
 ALTER TABLE public.app_user OWNER TO icfjttdivtiins;
-
---
--- TOC entry 207 (class 1259 OID 8442035)
--- Name: app_user_user_id_seq; Type: SEQUENCE; Schema: public; Owner: icfjttdivtiins
---
-
-ALTER TABLE public.app_user ALTER COLUMN user_id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.app_user_user_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
 
 --
 -- TOC entry 204 (class 1259 OID 8392192)
@@ -95,7 +80,7 @@ ALTER TABLE public.app_user ALTER COLUMN user_id ADD GENERATED ALWAYS AS IDENTIT
 CREATE TABLE public.company (
     name character varying(127) NOT NULL,
     headquarter_adress character varying(127),
-    user_id integer NOT NULL
+    company_uuid character varying(32) NOT NULL
 );
 
 
@@ -109,8 +94,8 @@ ALTER TABLE public.company OWNER TO icfjttdivtiins;
 CREATE TABLE public.person (
     first_name character varying(35) NOT NULL,
     last_name character varying(35) NOT NULL,
-    user_id integer NOT NULL,
-    credit_card_number character varying(16)
+    credit_card_number character varying(16) NOT NULL,
+    person_uuid character varying(32) NOT NULL
 );
 
 
@@ -123,7 +108,7 @@ ALTER TABLE public.person OWNER TO icfjttdivtiins;
 
 CREATE TABLE public.vehicle (
     registration_number character varying(8) NOT NULL,
-    user_id integer NOT NULL,
+    owner_uuid character varying(32) NOT NULL,
     CONSTRAINT vehicle_registration_number_check CHECK ((((registration_number)::text ~ '^[a-zA-Z\u0100-\u017F]{2}[0-9]{3,4}[a-zA-Z]{1,2}$'::text) OR ((registration_number)::text ~ '^[0-9]{6}$'::text)))
 );
 
@@ -131,81 +116,76 @@ CREATE TABLE public.vehicle (
 ALTER TABLE public.vehicle OWNER TO icfjttdivtiins;
 
 --
--- TOC entry 3871 (class 0 OID 8393287)
+-- TOC entry 3869 (class 0 OID 8393287)
 -- Dependencies: 205
 -- Data for Name: administrator; Type: TABLE DATA; Schema: public; Owner: icfjttdivtiins
 --
 
-COPY public.administrator (user_id) FROM stdin;
+COPY public.administrator (administrator_uuid) FROM stdin;
 \.
 
 
 --
--- TOC entry 3868 (class 0 OID 8392162)
+-- TOC entry 3866 (class 0 OID 8392162)
 -- Dependencies: 202
 -- Data for Name: app_user; Type: TABLE DATA; Schema: public; Owner: icfjttdivtiins
 --
 
-COPY public.app_user (email, password_hash, role, user_id, oib) FROM stdin;
-ivo.ivic@email.com	123ab44d2	person	55	12312312322
-ana.anic@email.com	123312321	person	60	00234225892
+COPY public.app_user (email, password_hash, role, oib, user_uuid) FROM stdin;
+ana.anic@fer.hr	12312312322	person	00000000002	123456789123456789123456789eeff5
+ivo.ivic@fer.hr	12321233232	person	00000000001	abcdefabcdefabcdefabcdefabcdef22
+firma1@gmail.com	5454454	company	00000000003	abcdefabcdefabcdefabc4efabcdef22
+admin.adminic@fer.hr	5455444134	administrator	\N	12345678912345678912da56789eeff5
+tin.tinic@fer.hr	12332132131213	person	00000000004	1234567891234567891fadc6789eeff5
+stipe.stipic@fer.hr	4545565654	person	00000000005	123456789123456789123456ff9eeff5
+firma@gmail.com	45742446462	company	00000000006	12345678912345678912ffe6789eeff5
+admin2@fer.hr	35t55555443	administrator	\N	123eee78912345678912ffe6789eeff5
+admin4@fer.hr	12323	administrator	\N	123456789123456789fffff6789eeff5
+tvrtka2@fer.hr	343434232	company	12312312322	123456789123456ffffffff6789eeff5
 \.
 
 
 --
--- TOC entry 3870 (class 0 OID 8392192)
+-- TOC entry 3868 (class 0 OID 8392192)
 -- Dependencies: 204
 -- Data for Name: company; Type: TABLE DATA; Schema: public; Owner: icfjttdivtiins
 --
 
-COPY public.company (name, headquarter_adress, user_id) FROM stdin;
+COPY public.company (name, headquarter_adress, company_uuid) FROM stdin;
 \.
 
 
 --
--- TOC entry 3869 (class 0 OID 8392177)
+-- TOC entry 3867 (class 0 OID 8392177)
 -- Dependencies: 203
 -- Data for Name: person; Type: TABLE DATA; Schema: public; Owner: icfjttdivtiins
 --
 
-COPY public.person (first_name, last_name, user_id, credit_card_number) FROM stdin;
-Ivo	Ivić	55	\N
+COPY public.person (first_name, last_name, credit_card_number, person_uuid) FROM stdin;
 \.
 
 
 --
--- TOC entry 3872 (class 0 OID 8393307)
+-- TOC entry 3870 (class 0 OID 8393307)
 -- Dependencies: 206
 -- Data for Name: vehicle; Type: TABLE DATA; Schema: public; Owner: icfjttdivtiins
 --
 
-COPY public.vehicle (registration_number, user_id) FROM stdin;
-ČK123OS	55
-ZG2234A	55
-ŠI121P	55
+COPY public.vehicle (registration_number, owner_uuid) FROM stdin;
 \.
 
 
 --
--- TOC entry 3882 (class 0 OID 0)
--- Dependencies: 207
--- Name: app_user_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: icfjttdivtiins
---
-
-SELECT pg_catalog.setval('public.app_user_user_id_seq', 87, true);
-
-
---
--- TOC entry 3735 (class 2606 OID 8442111)
+-- TOC entry 3733 (class 2606 OID 8790079)
 -- Name: administrator administrator_un; Type: CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
 ALTER TABLE ONLY public.administrator
-    ADD CONSTRAINT administrator_un UNIQUE (user_id);
+    ADD CONSTRAINT administrator_un UNIQUE (administrator_uuid);
 
 
 --
--- TOC entry 3725 (class 2606 OID 8442194)
+-- TOC entry 3723 (class 2606 OID 8442194)
 -- Name: app_user app_user_email_un; Type: CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
@@ -214,7 +194,7 @@ ALTER TABLE ONLY public.app_user
 
 
 --
--- TOC entry 3727 (class 2606 OID 8442950)
+-- TOC entry 3725 (class 2606 OID 8442950)
 -- Name: app_user app_user_oib_un; Type: CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
@@ -223,34 +203,34 @@ ALTER TABLE ONLY public.app_user
 
 
 --
--- TOC entry 3729 (class 2606 OID 8442051)
--- Name: app_user app_user_pk; Type: CONSTRAINT; Schema: public; Owner: icfjttdivtiins
+-- TOC entry 3727 (class 2606 OID 8787660)
+-- Name: app_user app_user_uuid_pk; Type: CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
 ALTER TABLE ONLY public.app_user
-    ADD CONSTRAINT app_user_pk PRIMARY KEY (user_id);
+    ADD CONSTRAINT app_user_uuid_pk PRIMARY KEY (user_uuid);
 
 
 --
--- TOC entry 3733 (class 2606 OID 8442063)
+-- TOC entry 3731 (class 2606 OID 8790081)
 -- Name: company company_un; Type: CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
 ALTER TABLE ONLY public.company
-    ADD CONSTRAINT company_un UNIQUE (user_id);
+    ADD CONSTRAINT company_un UNIQUE (company_uuid);
 
 
 --
--- TOC entry 3731 (class 2606 OID 8442072)
+-- TOC entry 3729 (class 2606 OID 8790083)
 -- Name: person person_un; Type: CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
 ALTER TABLE ONLY public.person
-    ADD CONSTRAINT person_un UNIQUE (user_id);
+    ADD CONSTRAINT person_un UNIQUE (person_uuid);
 
 
 --
--- TOC entry 3737 (class 2606 OID 8443026)
+-- TOC entry 3735 (class 2606 OID 8443026)
 -- Name: vehicle vehicle_un; Type: CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
@@ -259,43 +239,43 @@ ALTER TABLE ONLY public.vehicle
 
 
 --
--- TOC entry 3740 (class 2606 OID 8442112)
+-- TOC entry 3738 (class 2606 OID 8790069)
 -- Name: administrator administrator_fk; Type: FK CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
 ALTER TABLE ONLY public.administrator
-    ADD CONSTRAINT administrator_fk FOREIGN KEY (user_id) REFERENCES public.app_user(user_id) ON UPDATE CASCADE ON DELETE CASCADE;
+    ADD CONSTRAINT administrator_fk FOREIGN KEY (administrator_uuid) REFERENCES public.app_user(user_uuid) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
--- TOC entry 3739 (class 2606 OID 8442066)
+-- TOC entry 3737 (class 2606 OID 8790064)
 -- Name: company company_fk; Type: FK CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
 ALTER TABLE ONLY public.company
-    ADD CONSTRAINT company_fk FOREIGN KEY (user_id) REFERENCES public.app_user(user_id) ON UPDATE CASCADE ON DELETE CASCADE;
+    ADD CONSTRAINT company_fk FOREIGN KEY (company_uuid) REFERENCES public.app_user(user_uuid) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
--- TOC entry 3738 (class 2606 OID 8442073)
+-- TOC entry 3736 (class 2606 OID 8788589)
 -- Name: person person_fk; Type: FK CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
 ALTER TABLE ONLY public.person
-    ADD CONSTRAINT person_fk FOREIGN KEY (user_id) REFERENCES public.app_user(user_id) ON UPDATE CASCADE ON DELETE CASCADE;
+    ADD CONSTRAINT person_fk FOREIGN KEY (person_uuid) REFERENCES public.app_user(user_uuid) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
--- TOC entry 3741 (class 2606 OID 8442098)
+-- TOC entry 3739 (class 2606 OID 8790084)
 -- Name: vehicle vehicle_fk; Type: FK CONSTRAINT; Schema: public; Owner: icfjttdivtiins
 --
 
 ALTER TABLE ONLY public.vehicle
-    ADD CONSTRAINT vehicle_fk FOREIGN KEY (user_id) REFERENCES public.person(user_id) ON UPDATE CASCADE ON DELETE CASCADE;
+    ADD CONSTRAINT vehicle_fk FOREIGN KEY (owner_uuid) REFERENCES public.person(person_uuid) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
--- TOC entry 3880 (class 0 OID 0)
+-- TOC entry 3877 (class 0 OID 0)
 -- Dependencies: 3
 -- Name: SCHEMA public; Type: ACL; Schema: -; Owner: icfjttdivtiins
 --
@@ -307,15 +287,15 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --
--- TOC entry 3881 (class 0 OID 0)
--- Dependencies: 643
+-- TOC entry 3878 (class 0 OID 0)
+-- Dependencies: 641
 -- Name: LANGUAGE plpgsql; Type: ACL; Schema: -; Owner: postgres
 --
 
 GRANT ALL ON LANGUAGE plpgsql TO icfjttdivtiins;
 
 
--- Completed on 2020-11-02 15:32:31
+-- Completed on 2020-11-03 15:54:01
 
 --
 -- PostgreSQL database dump complete
